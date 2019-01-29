bdc246ecfcde3b5481050b88e7a699623051f4ba
Noah_Shan <shanwzh@inspur.com>
2019-01-29 11:27:50 +0800

    ====Noah Shan====
    交互处理完毕-
    
    ====END====

---
+++ flutterv1demo/ios/Runner/FlutterSwiftViewController.swift
@@ -0,0 +1,35 @@
+//
+//  *******************************************
+//  
+//  FlutterSwiftViewController.swift
+//  Runner
+//
+//  Created by Noah_Shan on 2019/1/29.
+//  Copyright © 2018 Inpur. All rights reserved.
+//  
+//  *******************************************
+//
+
+
+import UIKit
+
+class FlutterSwiftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
