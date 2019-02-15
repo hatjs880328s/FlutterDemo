3e7ce652ebebf940f8dff699662ee684f4f7093b
Noah_Shan <shanwzh@inspur.com>
2019-02-15 12:33:55 +0800

    ====Noah Shan====
    url修改
    
    ====END====

--- flutterv1demo/lib/Utility/IIHTTP/IIHTTPStaticInfos.dart
+++ flutterv1demo/lib/Utility/IIHTTP/IIHTTPStaticInfos.dart
@@ -4,5 +4,5 @@ class IIHTTPStaticInfos {
   static String tokenID = "51c6a0a8-7a40-47e8-b8bd-cc305ece9c52";
   static String tokenSecret = "a4738cc3-f929-46d2-b4e5-277b6cac8e01";
   static String tokenUrl = "https://id.inspuronline.com/oauth2.0/token";
-  static String iiexcLogin = "https://emm.inspur.com/api/ews/v1.0/UserProfile/MailBind";
+  static String iiexcLogin = "https://emm-inspur.inspuronline.com/api/ews/v1.0/UserProfile/MailBind";
 }
