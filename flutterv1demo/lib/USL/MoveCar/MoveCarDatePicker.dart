b41dc4d8e3a019773bee6257ce5ee9285333ce45
Noah_Shan <shanwzh@inspur.com>
2019-03-08 14:51:50 +0800

    ====Noah Shan====
    无法多级联动 - 目前
    
    ====END====

--- flutterv1demo/lib/USL/MoveCar/MoveCarDatePicker.dart
+++ flutterv1demo/lib/USL/MoveCar/MoveCarDatePicker.dart
@@ -9,9 +9,7 @@ class CupertinoPickerDemo extends StatefulWidget {
 
   static const String routeName = '/cupertino/picker';
 
-  
-
-  Widget showWidget;
+  final Widget showWidget;
 
   CupertinoPickerDemo(this.showWidget);
 
@@ -31,15 +29,14 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
   /// 网络获取的大数据结构
   List<dynamic> bigInfos;
 
+  /// 省信息
   List<String> provinces = [];
 
-  List<List<String>> cityCharList = [];
-
+  /// 获取网络数据并处理
   getDate() async {
     this.bigInfos = await bll.getProvinces();
     this.cityInfos = bll.progressCity(this.bigInfos);
     this.provinces = bll.progressProvinces(this.bigInfos);
-    
   }
 
 /// 创建两个picker
@@ -97,11 +94,13 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
   }
 
   Widget _buildColorPicker(BuildContext context) {
+
     final FixedExtentScrollController leftCon =
         FixedExtentScrollController(initialItem: _selectedColorIndex);
 
     final FixedExtentScrollController rightCon =
         FixedExtentScrollController(initialItem: _selectedColorIndex);
+
     return GestureDetector(
       onTap: () async {
         await this.getDate();
@@ -111,7 +110,7 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
             return _buildBottomPicker(
                 CupertinoPicker(
                   magnification: 1.0,
-                  scrollController:leftCon,
+                  //scrollController:leftCon,
                   itemExtent: _kPickerItemHeight,
                   backgroundColor: CupertinoColors.white,
                   useMagnifier: true,
@@ -128,18 +127,18 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
                 ),
                 CupertinoPicker(
                   magnification: 1.0,
-                  scrollController:rightCon,
+                  //scrollController:rightCon,
                   itemExtent: _kPickerItemHeight,
                   backgroundColor: CupertinoColors.white,
                   useMagnifier: true,
                   onSelectedItemChanged: (int index) {
                     // 当正中间选项改变时的回调
-                    setState(() => _selectedColorIndex = index);
+                    print("hello, second $index");
                   },
                   children:
-                    List<Widget>.generate(provinces.length, (int index) {
+                    List<Widget>.generate(cityInfos[_selectedColorIndex].length, (int index) {
                     return Center(
-                      child: Text(provinces[index]),
+                      child: Text(cityInfos[_selectedColorIndex][index].toString()),
                     );
                   }),
                 ));
@@ -147,7 +146,7 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
         );
       },
       child: Container(
-        child: widget.showWidget,//Text('hello'),
+        child: widget.showWidget,
       ),
     );
   }
@@ -155,6 +154,7 @@ class _CupertinoPickerDemoState extends State<CupertinoPickerDemo> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: _selectedColorIndex == 0 ? Colors.yellow : Colors.red,
       child: _buildColorPicker(context),
     );
   }
